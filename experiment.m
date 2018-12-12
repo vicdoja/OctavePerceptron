9c73cf07eba5135eb87493bb8d0fdd7ad52f1680
Víctor Dorado Javier <thevidoja@gmail.com>
2018-12-12 10:23:00 +0100

    Experiment v1.1

--- experiment.m
+++ experiment.m
@@ -14,21 +14,21 @@ load(data); #[N,L]=size(data); D=L-1;
 D = L-1;
 ll = unique(data(:, L));
 rand('seed', 23);
-data = data(randPerm(N));
+data = data(randperm(N), :);
 M = round(0.7*N); #M = number training data
 te = data(M+1:N, :); # te = testing data
 
-printf("#      a       b   E   k Ete Ete (%)    Ite (%)\n");
-printf("#------- ------- --- --- --- ------- -----------\n");
+printf("#      a        b   E   k Ete Ete (%%)    Ite (%%)\n");
+printf("#-------  ------- --- --- --- ------- -----------\n");
 
 for a=as
     for b=bs
         [w,E,k]=perceptron(data(1:M,:),b,a); rl=zeros(M,1);
         for n=1:(N-M) rl(n)=ll(linmach(w,[1 te(n,1:D)]')); end
         [nerr m]=confus(te(:,L),rl);
-        aprox_err = nerr/M;
-        interval = 1.96 * sqrt(aprox_err *(1-aprox_err)/(N-M))
+        aprox_err = nerr/(N-M);
+        interval = 1.96 * sqrt(aprox_err *(1-aprox_err)/(N-M));
         
-        printf("%8.1f %8.1f %3d %3d %3d %.3d [%.3f, %.3f]\n",a, b,E,k,nerr, aprox_err, aprox_err-interval, aprox_err+interval);
+        printf("%8.1f %8.3f %3d %3d %3d   %.2f [%.2f, %.2f]\n",a, b,E,k,nerr, aprox_err*100, (aprox_err-interval)*100, (aprox_err+interval)*100);
     end
 end
